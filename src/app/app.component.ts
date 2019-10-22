b806bae0bec891ef2fb016f555da438865e7c5bc
prudin <prudin@phoenixit.ru>
2019-10-22 12:46:04 +0300

    My initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {HeroService} from './hero-service/hero.service';
+import {MessageService} from './message-service/message.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService, MessageService, Router]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
 }
